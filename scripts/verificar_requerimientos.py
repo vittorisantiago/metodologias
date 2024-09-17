d02e3b3587703e89b533cc93aa869cf6c25c5f99
Santiago <62179159+vittorisantiago@users.noreply.github.com>
2024-09-17 19:24:34 -0300

    Update verificar_requerimientos.py

---
+++ scripts/verificar_requerimientos.py
@@ -0,0 +1,11 @@
+import json
+
+def verificar_requisitos():
+    with open('requerimientos.json') as f:
+        requisitos = json.load(f)
+    if requisitos['actualizados']:
+        return 0
+    return 1
+
+if __name__ == "__main__":
+    exit(verificar_requisitos())
